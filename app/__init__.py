e57b9c2521241840bc426a9a6993403812ed3213
Edward Escasio <eddieskywalker123@gmail.com>
2024-05-27 23:53:54 +0800

    Refactor structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,33 +1,31 @@
-from flask import Flask, render_template
-from flask_mysql_connector import MySQL
-from flask_bootstrap import Bootstrap
-from config import DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, BOOTSTRAP_SERVE_LOCAL
+from flask import Flask
+import mysql.connector
+from config import DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, SECRET_KEY, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET
 from flask_wtf.csrf import CSRFProtect
+import cloudinary
 
-mysql = MySQL()
-bootstrap = Bootstrap()
+# db = mysql.connector.connect(
+#     host=DB_HOST,
+#     user=DB_USER,
+#     password=DB_PASSWORD,
+#     database=DB_NAME
+# )
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY=SECRET_KEY,
-        MYSQL_USER=DB_USERNAME,
-        MYSQL_PASSWORD=DB_PASSWORD,
-        MYSQL_DATABASE=DB_NAME,
-        MYSQL_HOST=DB_HOST,
-        #BOOTSTRAP_SERVE_LOCAL=BOOTSTRAP_SERVE_LOCAL
-    )
-    bootstrap.init_app(app)
-    mysql.init_app(app)
-    CSRFProtect(app)
+cloudinary.config( 
+  cloud_name=CLOUDINARY_CLOUD_NAME, 
+  api_key=CLOUDINARY_API_KEY,
+  api_secret=CLOUDINARY_API_SECRET
+)
 
-    from .controller.students import student
-    app.register_blueprint(student)
+app = Flask(__name__, instance_relative_config=True)
+app.config['SECRET_KEY'] = SECRET_KEY
+CSRFProtect(app)
 
-    from .controller.courses import course
-    app.register_blueprint(course)
+# from .controller.students import student
+# app.register_blueprint(student)
 
-    from .controller.colleges import college
-    app.register_blueprint(college)
+# from .controller.courses import course
+# app.register_blueprint(course)
 
-    return app
+# from .controller.colleges import college
+# app.register_blueprint(college)
--- flask.sql
+++ flask.sql
@@ -1,29 +1,25 @@
-CREATE TABLE `colleges` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `code` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE colleges (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	code VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE `courses` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `code` varchar(50) NOT NULL,
-  `college_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `college_id` (`college_id`),
-  CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`college_id`) REFERENCES `colleges` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE courses (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	code VARCHAR(255) NOT NULL,
+	college_id INT NOT NULL,
+	CONSTRAINT fk_college_id FOREIGN KEY(college_id) REFERENCES colleges(id)
+);
 
-CREATE TABLE `students` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_number` varchar(255) DEFAULT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `course_id` int DEFAULT NULL,
-  `year` varchar(50) NOT NULL,
-  `gender` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `course_id` (`course_id`),
-  CONSTRAINT `students_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+CREATE TABLE students (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	avatar_url VARCHAR (255),
+	id_number VARCHAR(255) NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+	course_id INT NOT NULL,
+	year VARCHAR(255) NOT NULL,
+	gender VARCHAR (255) NOT NULL,
+	CONSTRAINT fk_course_id FOREIGN KEY(course_id) REFERENCES courses(id)
+);
